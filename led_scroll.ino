be22677d7b9aba9b0e8c86a5d911b24826191762
gsalaman <43499190+gsalaman@users.noreply.github.com>
2019-01-16 11:48:22 -0700

    Broken!!! Why does rows go to 1024????

--- led_scroll.ino
+++ led_scroll.ino
@@ -1,11 +1,13 @@
 /*=====================================
- * 2d dev:  checkpoint.  two 1-d arrays.
+ * 2d dev:  Now try to make a new array that holds the two 1-d arrays...
  */
 //////////////////////
 // Library Includes //
 //////////////////////
 #include <Adafruit_GFX.h>   // Core graphics library
 #include <RGBmatrixPanel.h> // Hardware-specific library
+
+#include "memprint.h"
 
 /////////////////////
 // Hardware Hookup //
@@ -42,6 +44,15 @@ int16_t color=matrix.Color333(1,0,0);
 #define BUFFER_SIZE 40
 uint8_t row0[BUFFER_SIZE]={1,0,1,1,0,1,1,1,0,1,1,1,1,0};
 uint8_t row1[BUFFER_SIZE]={1,0,1,1,0,1,1,1,0,1,1,1,1,0};
+uint8_t row2[BUFFER_SIZE]={1,0,1,1,0,1,1,1,0,1,1,1,1,0};
+uint8_t row3[BUFFER_SIZE]={1,0,1,1,0,1,1,1,0,1,1,1,1,0};
+uint8_t row4[BUFFER_SIZE]={1,0,1,1,0,1,1,1,0,1,1,1,1,0};
+uint8_t row5[BUFFER_SIZE]={1,0,1,1,0,1,1,1,0,1,1,1,1,0};
+uint8_t row6[BUFFER_SIZE]={1,0,1,1,0,1,1,1,0,1,1,1,1,0};
+uint8_t row7[BUFFER_SIZE]={1,0,1,1,0,1,1,1,0,1,1,1,1,0};
+
+#define NUM_ROWS 2
+uint8_t *rows[NUM_ROWS] = {row0, row1};
 
 int current_buffer_column = 0;
 
@@ -55,39 +66,56 @@ void setup()
 
   Serial.begin(9600);   // Start serial
 
+  memprint_bytes(row0);
+  memprint_bytes(row1);
+  memprint_bytes(rows);
+
   
 }
 
 void scroll_iteration( void )
 {
-  int matrix_column;
-  int buffer_column;
+  int8_t matrix_column;
+  int8_t buffer_column;
+  int8_t current_row;
+  uint8_t *row_ptr;
+
+  Serial.println("Enter scroll_iteration");
   
   // print, from left to right, the 10 characters from our current buffer column
   for (matrix_column = 0; matrix_column < WINDOW_SIZE; matrix_column++)
   {
     buffer_column = (matrix_column + current_buffer_column) % BUFFER_SIZE;
 
-    // cumbersome, but a decent test...
-    if (row0[buffer_column])
-    {
-      matrix.drawPixel(matrix_column, 0, color);
-    }
-    else
-    {
-      //yes, we do have to draw black here to erase anything previously drawn
-      matrix.drawPixel(matrix_column, 0, 0);
-    }
-    if (row1[buffer_column])
-    {
-      matrix.drawPixel(matrix_column, 1, color);
-    }
-    else
-    {
-      //yes, we do have to draw black here to erase anything previously drawn
-      matrix.drawPixel(matrix_column, 1, 0);
-    }
+    Serial.print("buffer_column=");
+    Serial.print(buffer_column);
+    Serial.print(" matrix_column=");
+    Serial.println(matrix_column);
 
+    for (current_row = 0; current_row < NUM_ROWS; current_row++)
+    {
+      row_ptr = rows[current_row];
+
+      Serial.print("pixel: ");
+      Serial.print(matrix_column);
+      Serial.print(",");
+      Serial.println(current_row);
+
+      //memprint_bytes(&current_row, 2);
+      
+      if (row_ptr[buffer_column])
+      {
+        matrix.drawPixel(matrix_column, current_row, color);
+      }
+      else
+      {
+        //yes, we do have to draw black here to erase anything previously drawn
+        matrix.drawPixel(matrix_column, current_row, 0);
+      }
+
+      //memprint_bytes(&current_row, 2);
+    }
+    
   }
 
   // next time through we're gonna want the next line, so increment current_buffer_column
