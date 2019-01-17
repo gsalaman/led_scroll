0f5d04efd15c2f4db1f3807ed8d5283a6b496261
gsalaman <43499190+gsalaman@users.noreply.github.com>
2019-01-16 17:21:58 -0700

    alt 2d approach.  No scroll yet

--- led_scroll.ino
+++ led_scroll.ino
@@ -7,8 +7,6 @@
 #include <Adafruit_GFX.h>   // Core graphics library
 #include <RGBmatrixPanel.h> // Hardware-specific library
 
-#include "memprint.h"
-
 /////////////////////
 // Hardware Hookup //
 /////////////////////
@@ -33,23 +31,11 @@
 ////////////////////////////////////////////////
 
 /* ========== For 32x32 LED panels: ========== */
-RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, false); // 32x32
-
-int8_t cursorX = 0;  // Cursor x position, initialize left
-int8_t cursorY = 0;  // Cursor y position, initialize top
-
-int16_t color=matrix.Color333(1,0,0);
 
 
 #define BUFFER_SIZE 40
-uint8_t row0[BUFFER_SIZE]={1,0,1,1,0,1,1,1,0,1,1,1,1,0};
-uint8_t row1[BUFFER_SIZE]={1,0,1,1,0,1,1,1,0,1,1,1,1,0};
-uint8_t row2[BUFFER_SIZE]={1,0,1,1,0,1,1,1,0,1,1,1,1,0};
-uint8_t row3[BUFFER_SIZE]={1,0,1,1,0,1,1,1,0,1,1,1,1,0};
-uint8_t row4[BUFFER_SIZE]={1,0,1,1,0,1,1,1,0,1,1,1,1,0};
-uint8_t row5[BUFFER_SIZE]={1,0,1,1,0,1,1,1,0,1,1,1,1,0};
-uint8_t row6[BUFFER_SIZE]={1,0,1,1,0,1,1,1,0,1,1,1,1,0};
-uint8_t row7[BUFFER_SIZE]={1,0,1,1,0,1,1,1,0,1,1,1,1,0};
+//uint8_t row0[BUFFER_SIZE]={1,1,1,1,0,1,1,1,0,1,1,1,1,0};
+//uint8_t row1[BUFFER_SIZE]={0,1,1,1,0,1,1,1,0,1,1,1,1,0};
 
 int current_buffer_column = 0;
 
@@ -57,42 +43,43 @@ int current_buffer_column = 0;
 #define WINDOW_START 0
 #define WINDOW_SIZE 32
 
+RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, false); // 32x32
+
 void setup()
 {
   matrix.begin();       // Initialize the matrix.
 
-  Serial.begin(9600);   // Start serial  
+  propagate_data();
+  
+  //Serial.begin(9600);   // Start serial  
 }
 
+#if 0
 void scroll_iteration( void )
 {
   int matrix_column;
   int buffer_column;
+  uint8_t *row0_ptr;
+  uint8_t *row1_ptr;
 
+  int16_t color;
+  int16_t blank;
+
+  color=matrix.Color333(1,0,0);
+  blank=matrix.Color333(0,0,0);
   
-  
-  // print, from left to right, the 10 characters from our current buffer column
   for (matrix_column = 0; matrix_column < WINDOW_SIZE; matrix_column++)
   {
     buffer_column = (matrix_column + current_buffer_column) % BUFFER_SIZE;
 
-    // BLEAH!!! 8 times, by hand????
-    if (row0[buffer_column]) matrix.drawPixel(matrix_column, 0, color);
-    else matrix.drawPixel(matrix_column, 0, 0);    
-    if (row1[buffer_column]) matrix.drawPixel(matrix_column, 1, color);
-    else matrix.drawPixel(matrix_column, 1, 0);
-    if (row2[buffer_column]) matrix.drawPixel(matrix_column, 2, color);
-    else matrix.drawPixel(matrix_column, 2, 0);
-    if (row3[buffer_column]) matrix.drawPixel(matrix_column, 3, color);
-    else matrix.drawPixel(matrix_column, 3, 0);
-    if (row4[buffer_column]) matrix.drawPixel(matrix_column, 4, color);
-    else matrix.drawPixel(matrix_column, 4, 0);
-    if (row5[buffer_column]) matrix.drawPixel(matrix_column, 5, color);
-    else matrix.drawPixel(matrix_column, 5, 0);
-    if (row6[buffer_column]) matrix.drawPixel(matrix_column, 6, color);
-    else matrix.drawPixel(matrix_column, 6, 0);
-    if (row7[buffer_column]) matrix.drawPixel(matrix_column, 7, color);
-    else matrix.drawPixel(matrix_column, 7, 0);
+    row0_ptr = row0 + buffer_column;
+    row1_ptr = row1 + buffer_column;
+
+    if (*row0_ptr) matrix.drawPixel(matrix_column, 0, color);
+    else matrix.drawPixel(matrix_column, 0, blank);    
+
+    if (*row1_ptr) matrix.drawPixel(matrix_column, 1, color);
+    else matrix.drawPixel(matrix_column, 1, blank);
     
   }
 
@@ -100,13 +87,51 @@ void scroll_iteration( void )
   current_buffer_column = (current_buffer_column + 1) % BUFFER_SIZE;
   
 }
+#endif
 
+bool data[32*2]={0};
+
+void propagate_data( void )
+{
+  int i;
+
+  for (i=0; i<32; i++)
+  {
+    if (i%2) data[i]=1;
+  }
+  for (i=32; i<64; i++)
+  {
+    if (i%3) data[i]=1;
+  }
+}
+
+void test_multiple_writes( void )
+{
+  int16_t color;
+  int16_t blank;
+  int x;
+  int y;
+
+  color = matrix.Color333(1,0,0);
+  blank = 0;
+
+  for (x=0; x<32; x++)
+  {
+    for (y=0; y<2; y++)
+    {
+      if ( data[((y*32)+x)] ) matrix.drawPixel(x, y, color);
+      else matrix.drawPixel(x, y, blank);
+    }
+  }
+  
+}
 void loop()
 {
-   scroll_iteration();
+   test_multiple_writes();
+   //scroll_iteration();
    
-   while (!Serial.available());
-   char c = Serial.read();
-   //delay(50);
+   //while (!Serial.available());
+   //char c = Serial.read();
+   delay(50);
    
 }
